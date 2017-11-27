1f7d3e55061aa2f183d2969f3ce2489d8afd39cc
zh17hai <zh15720623019@126.com>
2017-11-27 23:41:13 +0800

    电影管理（添加电影）

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,13 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
+import os
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:941003@127.0.0.1:3306/movie"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config["SECRET_KEY"] = '79b3aaa9b8054b799e5159c38a892dfc'
+app.config["UP_DIR"] = os.path.join(os.path.abspath(os.path.dirname(__file__)), "static/uploads/")
 app.debug = True
 db = SQLAlchemy(app)
 
