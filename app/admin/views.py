6756df5636bec6575f5d68c73c586fdbc18538e8
zh17hai <zh15720623019@126.com>
2017-11-29 23:09:30 +0800

    会员管理功能（查看，删除）（未测试）

--- app/admin/views.py
+++ app/admin/views.py
@@ -2,7 +2,7 @@
 from . import admin
 from flask import render_template, redirect, url_for, flash, session, request
 from app.admin.forms import LoginForm,TagForm, MovieForm, PreviewForm
-from app.models import Admin,Tag, Movie, Preview
+from app.models import Admin,Tag, Movie, Preview, User
 from functools import wraps
 from app import db,app
 from werkzeug.utils import secure_filename
@@ -296,15 +296,33 @@ def preview_edit(id=None):
         return redirect(url_for('admin.preview_edit', id=id))
     return render_template("admin/preview_edit.html", form=form, preview=preview)
 
-@admin.route("/user/list/")
+#会员列表
+@admin.route("/user/list/<int:page>/", methods=["GET"])
 @admin_login_req
-def user_list():
-    return render_template("admin/user_list.html")
+def user_list(page=None):
+    if page == None:
+        page = 1
+    page_data = User.query.order_by(
+        User.addtime.desc()
+    ).paginate(page=page, per_page=10)
+    return render_template("admin/user_list.html",page_data=page_data)
 
-@admin.route("/user/view/")
+#会员详情
+@admin.route("/user/view/<int:id>/", methods=["GET"])
 @admin_login_req
-def user_view():
-    return render_template("admin/user_view.html")
+def user_view(id=None):
+    user = User.query.get_or_404(int(id))
+    return render_template("admin/user_view.html", user=user)
+
+#会员删除
+@admin.route("/user/del/<int:id>/", methods=["GET"])
+@admin_login_req
+def user_del(id=None):
+    user = User.query.get_or_404(int(id))
+    db.session.delete(user)
+    db.session.commit()
+    flash("删除会员成功！", "ok")
+    return redirect(url_for('admin.user_list', page=1))
 
 @admin.route("/comment/list/")
 @admin_login_req
